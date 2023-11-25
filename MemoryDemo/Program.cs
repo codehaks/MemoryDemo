e965dabf7c9c54eb48eee6a6466b519047074eca
Hakim Ghods <ghods.hakim@hotmail.com>
2023-11-25 11:23:42 +0330

    Updated to .NET 8

--- MemoryDemo/Program.cs
+++ MemoryDemo/Program.cs
@@ -4,65 +4,59 @@ using System.IO;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace MemoryDemo
+
+
+Console.WriteLine("Press any key to start...");
+
+Console.WriteLine("Proccessor Id : {0}", Process.GetCurrentProcess().Id);
+
+Console.ReadLine();
+//await CopyFilesAsync();
+await StreamCopyFilesAsync();
+Console.ReadLine();
+Console.WriteLine("Done");
+
+
+
+static async Task StreamCopyFilesAsync()
 {
-    class Program
+    byte[] buffer = new byte[16 * 1024];
+    var files = Directory.GetFiles(@"G:\Projects\Data\Fake-Faces");
+
+    var c = 0;
+    foreach (var file in files)
     {
-        static async Task Main(string[] args)
+        Console.WriteLine(file);
+        long totalBytes = new FileInfo(file).Length;
+
+        using var output = File.Create(@"G:\Projects\Data\Fake-Faces2\" + c + ".jpg");
+
+        Stream input = new FileStream(file, FileMode.Open);
+        long totalReadBytes = 0;
+        int readBytes;
+
+        while ((readBytes = input.Read(buffer, 0, buffer.Length)) > 0)
         {
-            Console.WriteLine("Press any key to start...");
-
-            Console.WriteLine(Process.GetCurrentProcess().Id);
-
-            Console.ReadLine();
-            //await CopyFilesAsync();
-            await StreamCopyFilesAsync();
-            Console.ReadLine();
-            Console.WriteLine("Done");
-
+            await output.WriteAsync(buffer, 0, readBytes);
+            totalReadBytes += readBytes;
         }
 
-        private static async Task StreamCopyFilesAsync()
-        {
-            byte[] buffer = new byte[16 * 1024];
-            var files = System.IO.Directory.GetFiles(@"E:\Projects\Data\faces");
-
-            var c = 0;
-            foreach (var file in files)
-            {
-                Console.WriteLine(file);
-                long totalBytes = new FileInfo(file).Length;
-
-                using var output = System.IO.File.Create(@"E:\Projects\Data\faces2\" + c + ".jpg");
-
-                Stream input = new FileStream(file, FileMode.Open);
-                long totalReadBytes = 0;
-                int readBytes;
-
-                while ((readBytes = input.Read(buffer, 0, buffer.Length)) > 0)
-                {
-                    await output.WriteAsync(buffer, 0, readBytes);
-                    totalReadBytes += readBytes;
-                }
-
-                c++;
+        c++;
 
 
 
-            }
-        }
+    }
+}
 
-        private static async Task CopyFilesAsync()
-        {
-            var files = System.IO.Directory.GetFiles(@"E:\Projects\Data\faces");
-            var c = 0;
-            foreach (var file in files)
-            {
-                Console.WriteLine(file);
-                var data = await File.ReadAllBytesAsync(file);
-                await File.WriteAllBytesAsync(@"E:\Projects\Data\faces2\" + c + ".jpg", data);
-                c++;
-            }
-        }
+static async Task CopyFilesAsync()
+{
+    var files = System.IO.Directory.GetFiles(@"E:\Projects\Data\faces");
+    var c = 0;
+    foreach (var file in files)
+    {
+        Console.WriteLine(file);
+        var data = await File.ReadAllBytesAsync(file);
+        await File.WriteAllBytesAsync(@"E:\Projects\Data\faces2\" + c + ".jpg", data);
+        c++;
     }
 }
